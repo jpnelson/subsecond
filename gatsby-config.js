bbe00bb188dc0d5a0fb89fe44b37548436d968ce
Joshua Nelson <jonelson@atlassian.com>
2019-03-16 08:33:50 -0700

    Remove old GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,12 +59,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-gtag`,
       options: {
         // your google analytics tracking id
