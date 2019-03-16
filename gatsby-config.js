fcf5ca9526fecbe497fd827029a13b49c934bb57
Joshua Nelson <jonelson@atlassian.com>
2019-03-16 08:16:49 -0700

    remove do not track paths

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Enables Google Optimize using your container Id
         // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
         // // Enables Google Optimize Experiment ID
